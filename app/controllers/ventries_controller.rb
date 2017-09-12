f195cc9f5169fa76d91fc4b4b6e547844c144b22
teddottavio <teddottavio@yahoo.com>
2017-09-12 14:02:41 -0400

    ventry is processed works. rethink interface

--- app/controllers/ventries_controller.rb
+++ app/controllers/ventries_controller.rb
@@ -11,6 +11,7 @@ class VentriesController < ApplicationController
     @showname =  Performance.find(show.first.performance_id).name
     @showid =  show.first.performance_id
 
+    #TODO We know the logtype... so categories for headset/loop vs. SS languages
     @productCats = []
     productCats = Shortlists.new.productCategories
     productCats.each do |key, value|
@@ -29,11 +30,11 @@ class VentriesController < ApplicationController
 
   def new
     #submit data - and reload ventries#index
-    flash[:success] = "" + params[:task] + "<br>"
+    # flash[:success] = "" + params[:task] + "<br>"
+    #TODO rethink this.. adding languages needs a check for last one:Done
 
     case params[:task]
     when "Data"
-      flash[:success] += "Done - week of #{params[:mystart].to_date.strftime('%b %d')} for show #{params[:showid]} Added.<br>"
       for i in 0..(params[:shiftcount].to_i)-1 do
         obj = Distributed.new()
         obj.performance_id = params[:showid].to_i
@@ -54,33 +55,34 @@ class VentriesController < ApplicationController
         obj.curtain = params[:mystart].to_date + params[rowname][:weekday].to_i
         obj.eve = params[rowname][:eve]
         obj.quantity = params[rowname][:qty]
-        # obj.save
+        obj.save
       end
       # Add isprocesed flag to scans
       scanobj = Scan.find(params[:myscan])
       scanobj.isprocessed = true
-      # flash[:success] += "---scans found : "
-      # flash[:success] += scanobj.isprocessed.to_s + "<br>"
       # scanobj.save
+      flash[:success] = "Week of #{params[:mystart].to_date.strftime('%b %d')} for show #{params[:showid]} Added.<br>"
     when "Blank"
       if params[:productCat].to_i <= 20 then
-        flash[:success] += "Enter 0 values for Infrared Shifts<br>"
+        flash[:error] = "Enter 0 values for Infrared Shifts<br>"
       else
         scanobj = Scan.find(params[:myscan])
         scanobj.isprocessed = true
-        # scanobj.save
+        scanobj.save
+        flash[:success] = "Log marked as processed<br>"
       end
     when "Move"
+      scanobj = Scan.find(params[:myscan])
       ss = 1
-      ss = 2 if params[:productCat].to_i > 20
-      movefile = findlog(params[:mystart], params[:showid], ss)
-      flash[:success] += "moving : " + movefile.to_s + "<br>"
+      ss = 2 if scanobj.specialservices
+      movefile = findlog(scanobj.monday, scanobj.id, ss)
+      flash[:success] = "Moved : " + movefile.to_s + "<br>"
       # require 'fileutils'
       # FileUtils.mv myfile, myplacedpath + myplacedfile
     # when "Extra"
     end
 
-    flash[:success] += i.to_s + " for this:   " + params.inspect
+    # flash[:success] += i.to_s + " for this:   " + params.inspect
 
     redirect_to ventry_path
   end

b8f4ef324fa78d988fbcba55349c01393397fabe
teddottavio <teddottavio@yahoo.com>
2017-11-09 19:20:44 -0500

    layout and css for index

--- app/controllers/distributeds_controller.rb
+++ app/controllers/distributeds_controller.rb
@@ -1,5 +1,63 @@
 class DistributedsController < ApplicationController
   def index
+    administratorsOnly
+
+    @mystart = if params[:mystart]
+                 params[:mystart].to_date
+               else
+                 weekstart
+               end
+
+    @weekof = @mystart.strftime('%b %d') + ' to ' + (@mystart + 6).strftime('%b %d, %Y')
+
+    performances = Performance.showinglist(@mystart)
+    @showstoedit = []
+    performances.each do |p|
+      mycount = Distributed.datespan(@mystart, (@mystart+7)).where('performance_id = ?', p.id).count
+      if mycount != 0 then
+        @showstoedit.push([p.name[0..16],p.id,"btn btn-sm btn-primary"])
+      else
+        @showstoedit.push([p.name[0..16],p.id,"btn btn-sm btn-danger"])
+      end
+    end
+
+    #Display where the data is at - overview of what has been entered
+    @weektoedit = Array.new
+    @weekstartstoedit = []
+
+    if params[:myweekslist]
+     weekslist = params[:myweekslist].to_i
+    else
+     weekslist = 8
+    end
+
+
+    for i in 1..weekslist do
+      mystart = weekstart - (i * 7)
+
+      #add in some info about what has been entered
+      myshowcount = Performance.showingcount(mystart, (mystart+6))
+      myinfraredcount = Distributed.infraredwkcount(mystart)
+      myscanscount = Scan.scanscount(mystart)
+      myssscanscount = Scan.ssscanscount(mystart)
+
+      if myinfraredcount == 0 then #&& myspecialservicescount == 0 then
+        mybuttonclass = "btn btn-sm btn-danger"
+      else
+        if myinfraredcount < myshowcount then
+          mybuttonclass = "btn btn-sm btn-warning"
+        else
+          mybuttonclass = "btn btn-sm btn-primary"
+        end
+      end
+
+      @weektoedit.push({"showweekof" => mystart.strftime('%b %d')+" to "+ (mystart+7).strftime('%b %d, %Y'),
+        "startdate" => mystart,
+        "showcount" => myshowcount,
+        "infraredcount" => myinfraredcount,
+        "scanscount" => myscanscount.to_s + " - " + myssscanscount.to_s,
+        "buttonclass" => mybuttonclass})
+    end
   end
 
   def show
